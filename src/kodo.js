17e9a6803771102c71322972fab767e546b974a6
kodo <kodo@exemplo.com>
2016-06-18 21:38:45 -0300

    naccib

--- src/kodo.js
+++ src/kodo.js
@@ -1,1 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {  document.getElementById("kodoforum").addEventListener("click", function(){	window.open("http://fabricadenoobs.boards.net/","","");  });    document.getElementById("kodocanal").addEventListener("click", function(){	window.open("https://www.youtube.com/channel/UCGObNjkNjo1OUPLlm8BTb3A/","","");  });    document.getElementById("kodosite").addEventListener("click", function(){	window.open("http://fabricadenoobs.wix.com/home","","");  });});
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("kodoforum").addEventListener("click", function(){
+	window.open("http://fabricadenoobs.boards.net/","","");
+  });
+  
+  document.getElementById("kodocanal").addEventListener("click", function(){
+	window.open("https://www.youtube.com/channel/UCGObNjkNjo1OUPLlm8BTb3A/","","");
+  });
+  
+  document.getElementById("kodosite").addEventListener("click", function(){
+	window.open("http://fabricadenoobs.wix.com/home","","");
+  });
+    
+  document.getElementById("kodotwitter").addEventListener("click", function(){
+	window.open("https://twitter.com/_natanael666","","");
+  });
+});
