1230aeb2a792914f076d13fbba064dbeeb3f7be4
kodo <kodo@exemplo.com>
2016-06-21 21:38:53 -0300

    rss canal

--- src/kodo.js
+++ src/kodo.js
@@ -14,4 +14,27 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("kodotwitter").addEventListener("click", function(){
 	window.open("https://twitter.com/_natanael666","","");
   });
+  
+  
+  
+  fbnoobs_req = new XMLHttpRequest();
+	fbnoobs_req.open("GET","https://www.youtube.com/channel/UCGObNjkNjo1OUPLlm8BTb3A/videos",false);
+	fbnoobs_req.send();
+	fbreg = /yt-lockup-title.*accessible-description/mg;
+	fbregurl = /watch\?v.*?<\/a>/m;
+	kcont = 0;
+	document.getElementById("videos").innerHTML = "";
+	while(reto = fbreg.exec(fbnoobs_req.responseText)){
+	    if(kcont == 5){
+		   break;
+		}
+		link = fbregurl.exec(reto);
+		
+		
+		
+		document.getElementById("videos").innerHTML = document.getElementById("videos").innerHTML + "<a src=https://www.youtube.com/watch?v=" + link + "<br>";
+		kcont++;
+	}
+  
+  
 });
